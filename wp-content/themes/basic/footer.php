024a2cbce8df7c2ffab84f2e098b59666bfe71ef
Jonathan Sewell <jonathanssewell@gmail.com>
2010-01-18 17:42:55 +0000

    ADDED Basic template

---
+++ wp-content/themes/basic/footer.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Default_Theme
+ */
+?>
+
+<div id="footer" role="contentinfo">
+	<p>
+		Powered by <a href="http://wordpress.org/">WordPress</a>
+	</p>
+</div>
+</div>
+
+<?php ?>
+		<?php wp_footer(); ?>
+</body>
+</html>
