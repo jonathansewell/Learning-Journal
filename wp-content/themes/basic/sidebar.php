dd559335cd4e90a22efbc3a717e70e004ce9fe6c
Jonathan Sewell <jonathanssewell@gmail.com>
2010-08-20 06:26:41 +0000

    Update from live

--- wp-content/themes/basic/sidebar.php
+++ wp-content/themes/basic/sidebar.php
@@ -8,10 +8,9 @@
     <h2 id="about-me">About Me</h2>
 
     <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elementum feugiat ligula, sed auctor dolor mollis
-        at. Nam enim massa, rhoncus eu viverra in, scelerisque in sapien. Praesent risus metus, tempus non pulvinar sed,
-        hendrerit et urna. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-        Suspendisse eleifend adipiscing turpis, vel elementum nunc accumsan sit amet.
+        I'm a web developer living in Brighton, UK. I work with .NET content managment systems and  
+		develop new systems using agile engineering practices and principles. I work at 
+		<a href="http://www.cubeworks.co.uk/">Cubeworks</a>.
     </p>
 </div>
 
